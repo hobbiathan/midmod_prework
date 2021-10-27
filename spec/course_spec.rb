8b2e792ed6ca2ca2cef963ac2a782ba2cc6f1b2c
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-27 10:05:00 -0600

    Add enroll method test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -22,4 +22,24 @@ RSpec.describe 'Course' do
     expect(course.students).to eq([])
   end
 
+  it 'can check capacity with full? (empty)' do
+    course = Course.new("Calculus", 2)
+    expect(course.full?).to be false
+  end
+
+  # I should've written the enroll before full?
+
+  it 'can push student object to students array' do
+    course = Course.new("Calculus", 2)
+    student_1 = Student.new({name: "Morgan", age: 21})
+    student_2 = Student.new({name: "Jordan", age: 29})
+
+    course.enroll(student_1)
+    course.enroll(student_2)
+
+    expected_array = [student_1, student_2]
+    expect(course.students).to eq(expected_array)
+  end
+
+
 end
