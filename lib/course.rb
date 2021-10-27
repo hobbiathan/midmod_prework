085b4d27a8aba8721e412396a406587502fdffa3
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-27 10:04:52 -0600

    Add enroll method

--- lib/course.rb
+++ lib/course.rb
@@ -7,4 +7,17 @@ class Course
     @students = []
   end
 
+  def full?
+    if @students.size <= @capacity
+      false
+    elsif @students.size >= @capacity
+      true
+    else
+      puts "I don't know how, but you got a negative capacity."
+    end
+  end
+
+  def enroll(student_obj)
+    @students << student_obj
+  end
 end
