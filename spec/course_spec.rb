4ec6c020c756023c770873c452e3cd65a4ccde37
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-27 09:54:57 -0600

    Add read course name test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,8 +3,13 @@ require './lib/student'
 
 RSpec.describe 'Course' do
   it 'is an instance of Course' do
-    course = Course.new()
+    course = Course.new("Calculus", 2)
     expect(course).to be_instance_of(Course)
+  end
+
+  it 'can read a Course name' do
+    course = Course.new("Calculus", 2)
+    expect(course.name).to eq("Calculus")
   end
 
 end
