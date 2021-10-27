d9eb6369ccce6e0ef207ebce99c8d2bc45d4b76f
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-27 09:33:22 -0600

    Add store/read hash test, read name key value test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,8 +2,20 @@ require './lib/student'
 
 RSpec.describe 'Student' do
   it 'is an instance of student' do
-    student = Student.new()
+    student = Student.new({name: "Morgan", age: 21})
     expect(student).to be_instance_of(Student)
+  end
+
+  it 'can store and read a given hash' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.inputted_hash).to eq({name: "Morgan", age: 21})
+  end
+
+  it 'can read a given hash :name keys value' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.name).to eq("Morgan")
   end
 
 
