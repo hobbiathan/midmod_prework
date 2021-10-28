4ef90f313fc0c1ca99b1a9b947bc1cf81ce481d1
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-28 10:36:11 -0600

    Create call_students method test

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -47,8 +47,20 @@ RSpec.describe 'Course' do
 
     course.enroll(student_1)
     course.enroll(student_2)
-    
+
     expect(course.full?).to be true
+  end
+
+  it 'can call student names from a given course' do
+    course = Course.new("Calculus", 2)
+    student_1 = Student.new({name: "Morgan", age: 21})
+    student_2 = Student.new({name: "Jordan", age: 29})
+
+    course.enroll(student_1)
+    course.enroll(student_2)
+
+    expected_array = [student_1.name, student_2.name]
+    expect(course.call_students).to eq(expected_array)
   end
 
 end
