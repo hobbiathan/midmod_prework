7b98969586c978fa4d21326dcd34efb9bb70ad8b
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-28 10:08:59 -0600

    Add name/course attribute reader tests

--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -2,8 +2,18 @@ require './lib/term'
 
 RSpec.describe 'Term' do
   it 'is an instance of Turn' do
-    turn = Turn.new()
-    expect(turn).to be_instance_of(Turn)
+    term = Term.new("Winter 2018")
+    expect(term).to be_instance_of(Term)
+  end
+
+  it 'can read Term name' do
+    term = Term.new("Winter 2018")
+    expect(term.name).to eq("Winter 2018")
+  end
+
+  it 'can initialize an empty array (courses)' do
+    term = Term.new("Winter 2018")
+    expect(term.courses).to eq([])
   end
 
 
